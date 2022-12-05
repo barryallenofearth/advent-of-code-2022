ff16b0a7cc2acd51f24ff78d0683714da459f40c
Lisa_und_Michi <lordrevan1999@web.de>
2022-12-05 09:04:43 +0100

    AOC-2022 05-12/1 start riddle 5/1

---
+++ src/main/java/de/barryallenofearth/adventofcode2022/riddle/e/stacks/part1/Main.java
@@ -0,0 +1,13 @@
+package de.barryallenofearth.adventofcode2022.riddle.e.stacks.part1;
+
+import de.barryallenofearth.adventofcode2022.riddle.e.stacks.common.util.StackInstructionsReader;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		StackInstructionsReader.readStacks("riddle-5.txt");
+	}
+}
