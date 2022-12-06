0b6f0d4547a9a01673147bfc3d077bd6b4ca0843
Michael Evelt <m.evelt@lvm.de>
2022-12-06 15:14:55 +0100

    AOC-2022 06-12 cleanup

--- src/main/java/de/barryallenofearth/adventofcode2022/riddle/f/signals/part2/Main_6_2.java
+++ src/main/java/de/barryallenofearth/adventofcode2022/riddle/f/signals/part2/Main_6_2.java
@@ -1,7 +1,7 @@
 package de.barryallenofearth.adventofcode2022.riddle.f.signals.part2;
 
-import de.barryallenofearth.adventofcode2022.riddle.f.signals.common.util.SignalStreamReader;
 import de.barryallenofearth.adventofcode2022.riddle.f.signals.common.util.StartSignalFinder;
+import de.barryallenofearth.adventofcode2022.riddle.util.RiddleFileReader;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -9,7 +9,8 @@ import java.util.List;
 
 public class Main_6_2 {
     public static void main(String[] args) throws IOException, URISyntaxException {
-        final List<String> strings = SignalStreamReader.readSignal();
+
+        final List<String> strings = RiddleFileReader.readAllLines("riddle-6.txt");
         final List<Integer> numberOfReadSymbolToEndOfStartSequence = StartSignalFinder.findNumberOfReadSymbolToEndOfStartSequence(strings, 14);
         for (Integer integer : numberOfReadSymbolToEndOfStartSequence) {
             System.out.println(integer);
