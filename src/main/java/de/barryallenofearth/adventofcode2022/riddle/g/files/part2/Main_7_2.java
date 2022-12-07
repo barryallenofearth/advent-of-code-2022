5fc00746b12d6a6eba1a9f5380754ea4cb486968
Michael Evelt <m.evelt@lvm.de>
2022-12-07 11:33:09 +0100

    AOC-2022 07-12/2 solution riddle 7 part 2

--- src/main/java/de/barryallenofearth/adventofcode2022/riddle/g/files/part2/Main_7_2.java
+++ src/main/java/de/barryallenofearth/adventofcode2022/riddle/g/files/part2/Main_7_2.java
@@ -3,25 +3,17 @@ package de.barryallenofearth.adventofcode2022.riddle.g.files.part2;
 import de.barryallenofearth.adventofcode2022.riddle.g.files.common.CalculateSizeInDirectory;
 import de.barryallenofearth.adventofcode2022.riddle.g.files.common.ReadDirectoryTree;
 import de.barryallenofearth.adventofcode2022.riddle.g.files.model.Directory;
-import de.barryallenofearth.adventofcode2022.riddle.g.files.part2.common.GetAllDirectoriesBelowSizeLimit;
+import de.barryallenofearth.adventofcode2022.riddle.g.files.part2.common.FindSmallestDirectoryWithSufficientSize;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main_7_2 {
     public static void main(String[] args) throws IOException, URISyntaxException {
         final List<Directory> directoryList = ReadDirectoryTree.readDirectoryTree();
-        final List<Directory> mostOuterDirBelowSizeLimit = GetAllDirectoriesBelowSizeLimit.getMostOuterDirBelowSizeLimit(100_000, directoryList, new ArrayList<>());
+        final Directory directory = FindSmallestDirectoryWithSufficientSize.findDirectory(directoryList);
 
-
-        int totalSize = 0;
-        for (Directory directory : mostOuterDirBelowSizeLimit) {
-            final int size = CalculateSizeInDirectory.calculateSize(directory);
-            totalSize += size;
-        }
-
-        System.out.println("A total size of " + totalSize + " can be deleted.");
+        System.out.println("A total size of " + CalculateSizeInDirectory.calculateSize(directory) + " can be deleted.");
     }
 }
