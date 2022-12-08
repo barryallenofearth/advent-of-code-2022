349f7ea1367104289e53656094e4ad247352c6e7
Michael Evelt <m.evelt@lvm.de>
2022-12-08 11:16:07 +0100

    AOC-2022 08-12/1 solution riddle 8 part 1

---
+++ src/main/java/de/barryallenofearth/adventofcode2022/riddle/h/treehouse/part2/Main_8_2.java
@@ -0,0 +1,12 @@
+package de.barryallenofearth.adventofcode2022.riddle.h.treehouse.part2;
+
+import de.barryallenofearth.adventofcode2022.riddle.h.treehouse.common.GridReader;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class Main_8_2 {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        final int[][] treeGrid = GridReader.readTreeGrid();
+    }
+}
