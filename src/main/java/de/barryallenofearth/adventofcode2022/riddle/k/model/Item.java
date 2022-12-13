dcac532917d7ec6a9de036250acddb518b4b0f13
Michael Evelt <m.evelt@lvm.de>
2022-12-13 09:03:01 +0100

    AoC-2022 11-12/1 start part 1

---
+++ src/main/java/de/barryallenofearth/adventofcode2022/riddle/k/model/Item.java
@@ -0,0 +1,10 @@
+package de.barryallenofearth.adventofcode2022.riddle.k.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Item {
+    private int worryLevel;
+}
--- src/main/java/de/barryallenofearth/adventofcode2022/riddle/k/part1/Main_11_1.java
+++ src/main/java/de/barryallenofearth/adventofcode2022/riddle/k/part1/Main_11_1.java
@@ -1,7 +1,17 @@
 package de.barryallenofearth.adventofcode2022.riddle.k.part1;
 
-public class Main_11_1 {
-    public static void main(String[] args) {
+import de.barryallenofearth.adventofcode2022.riddle.k.common.ReadInitialMonkeyList;
+import de.barryallenofearth.adventofcode2022.riddle.k.model.Monkey;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class Main_11_1 {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        final List<Monkey> monkeys = ReadInitialMonkeyList.readMonkeys();
+        for (Monkey monkey : monkeys) {
+            System.out.println(monkey);
+        }
     }
 }
