06f701b911706b130f8bdd50cb46ab4cf74d887d
Michael Evelt <m.evelt@lvm.de>
2022-12-22 07:59:10 +0100

    AOC-2022 17-12/1 shape movement is working

--- src/main/java/de/barryallenofearth/adventofcode2022/riddle/q/rocks/common/model/RockType.java
+++ src/main/java/de/barryallenofearth/adventofcode2022/riddle/q/rocks/common/model/RockType.java
@@ -3,34 +3,34 @@ package de.barryallenofearth.adventofcode2022.riddle.q.rocks.common.model;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Set;
+import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
 public enum RockType {
 
-	HORIZONTAL_BAR(Set.of(new Coordinates(0, 0),
+	HORIZONTAL_BAR(List.of(new Coordinates(0, 0),
 			new Coordinates(1, 0),
 			new Coordinates(2, 0),
 			new Coordinates(3, 0))),
-	CROSS(Set.of(new Coordinates(1, 0),
+	CROSS(List.of(new Coordinates(1, 0),
 			new Coordinates(0, 1),
 			new Coordinates(1, 1),
 			new Coordinates(2, 1),
 			new Coordinates(1, 2))),
-	ANGLE(Set.of(new Coordinates(0, 0),
+	ANGLE(List.of(new Coordinates(0, 0),
 			new Coordinates(1, 0),
 			new Coordinates(2, 0),
 			new Coordinates(0, 1),
 			new Coordinates(0, 2))),
-	VERTICAL_BAR(Set.of(new Coordinates(0, 0),
+	VERTICAL_BAR(List.of(new Coordinates(0, 0),
 			new Coordinates(0, 1),
 			new Coordinates(0, 2),
 			new Coordinates(0, 3))),
-	SQUARE(Set.of(new Coordinates(0, 0),
+	SQUARE(List.of(new Coordinates(0, 0),
 			new Coordinates(1, 0),
 			new Coordinates(0, 1),
 			new Coordinates(1, 1)));
 
-	private final Set<Coordinates> components;
+	private final List<Coordinates> components;
 }
--- src/main/java/de/barryallenofearth/adventofcode2022/riddle/q/rocks/part1/Main_17_1.java
+++ src/main/java/de/barryallenofearth/adventofcode2022/riddle/q/rocks/part1/Main_17_1.java
@@ -1,5 +1,9 @@
 package de.barryallenofearth.adventofcode2022.riddle.q.rocks.part1;
 
+import de.barryallenofearth.adventofcode2022.riddle.q.rocks.common.model.Coordinates;
+import de.barryallenofearth.adventofcode2022.riddle.q.rocks.common.model.MoveType;
+import de.barryallenofearth.adventofcode2022.riddle.q.rocks.common.model.RockType;
+import de.barryallenofearth.adventofcode2022.riddle.q.rocks.common.model.Shape;
 import de.barryallenofearth.adventofcode2022.riddle.util.RiddleFileReader;
 
 import java.io.IOException;
@@ -7,6 +11,32 @@ import java.net.URISyntaxException;
 
 public class Main_17_1 {
 
-	public static void main(String[] args) throws IOException, URISyntaxException {
-	}
+    public static void main(String[] args) throws IOException, URISyntaxException {
+
+
+        for (MoveType moveType : MoveType.values()) {
+            final Shape cross = new Shape(new Coordinates(1, 1), RockType.CROSS);
+            cross.move(moveType);
+            printShape(cross);
+            System.out.println();
+            cross.reverse(moveType);
+            printShape(cross);
+            System.out.println();
+        }
+
+    }
+
+    private static void printShape(Shape cross) {
+        for (int y = 6; y >= 0; y--) {
+            for (int x = 0; x < 7; x++) {
+                final Coordinates currentCoordinate = new Coordinates(x, y);
+                if (cross.getComponents().contains(currentCoordinate)) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println(y);
+        }
+    }
 }
