e0e455a206bdca1ad49c8524c533fbbb95e09af3
Lisa_und_Michi <lordrevan1999@web.de>
2023-01-05 08:26:28 +0100

    AOC-2022 24/1 example working

--- src/main/java/de/barryallenofearth/adventofcode2022/riddle/day24/blizzard/part1/ValleyWalker.java
+++ src/main/java/de/barryallenofearth/adventofcode2022/riddle/day24/blizzard/part1/ValleyWalker.java
@@ -22,6 +22,7 @@ public class ValleyWalker {
 
 		final Stack<ValleyAndExpeditionState> openNodes = new Stack<>();
 		openNodes.add(start);
+		printState(valley, start);
 
 		int numberOfNodesEvaluated = 0;
 		while (!openNodes.isEmpty()) {
@@ -57,33 +58,47 @@ public class ValleyWalker {
 				} else if (availableOptions.size() == 1) {
 					currentState.getExpeditionLocation().setX(availableOptions.get(0).getX());
 					currentState.getExpeditionLocation().setY(availableOptions.get(0).getY());
+					if (currentState.getBlizzardList().stream().noneMatch(blizzard -> blizzard.getCoordinates().equals(currentState.getExpeditionLocation()))) {
+						createNewBranch(openNodes, currentState, availableOptions, new Coordinates(currentState.getExpeditionLocation().getX(), currentState.getExpeditionLocation().getY()));
+					}
+
 				} else {
 					currentState.getExpeditionLocation().setX(availableOptions.get(0).getX());
 					currentState.getExpeditionLocation().setY(availableOptions.get(0).getY());
 					for (int possibility = 1; possibility < availableOptions.size(); possibility++) {
-						final ValleyAndExpeditionState next = new ValleyAndExpeditionState();
-						next.setMinute(currentState.getMinute() + 1);
-						next.setExpeditionLocation(availableOptions.get(possibility));
-						next.getBlizzardList().addAll(currentState.getBlizzardList().stream()
-								.map(blizzard -> new Blizzard(new Coordinates(blizzard.getCoordinates().getX(), blizzard.getCoordinates().getY()), blizzard.getDirection()))
-								.collect(Collectors.toList()));
-						openNodes.add(next);
+						createNewBranch(openNodes, currentState, availableOptions, availableOptions.get(possibility));
+					}
+					if (currentState.getBlizzardList().stream().noneMatch(blizzard -> blizzard.getCoordinates().equals(currentState.getExpeditionLocation()))) {
+						createNewBranch(openNodes, currentState, availableOptions, new Coordinates(currentState.getExpeditionLocation().getX(), currentState.getExpeditionLocation().getY()));
 					}
 				}
 				currentState.setMinute(currentState.getMinute() + 1);
+				//printState(valley, start);
 			}
 			numberOfNodesEvaluated++;
 			if (numberOfNodesEvaluated % 1_000 == 0) {
 				System.out.println(numberOfNodesEvaluated + " branches evaluated.");
 			}
-			if (currentState.getMinute() < minimumMinutes) {
+
+			if (currentState.getMinute() < minimumMinutes && currentState.getExpeditionLocation().equals(valley.getExit())) {
 				minimumMinutes = currentState.getMinute();
 				minState = currentState;
 				System.out.println(minimumMinutes + " is the new best time.");
 			}
 		}
+		System.out.println(numberOfNodesEvaluated + " branches evaluated.");
 
 		return minState;
+	}
+
+	private void createNewBranch(Stack<ValleyAndExpeditionState> openNodes, ValleyAndExpeditionState currentState, List<Coordinates> availableOptions, Coordinates coordinates) {
+		final ValleyAndExpeditionState next = new ValleyAndExpeditionState();
+		next.setMinute(currentState.getMinute() + 1);
+		next.setExpeditionLocation(coordinates);
+		next.getBlizzardList().addAll(currentState.getBlizzardList().stream()
+				.map(blizzard -> new Blizzard(new Coordinates(blizzard.getCoordinates().getX(), blizzard.getCoordinates().getY()), blizzard.getDirection()))
+				.collect(Collectors.toList()));
+		openNodes.add(next);
 	}
 
 	private void wrapBlizzardMovement(Valley valley, Blizzard blizzard) {
@@ -101,5 +116,29 @@ public class ValleyWalker {
 			blizzard.getCoordinates().setY(valley.getMinY());
 			break;
 		}
+	}
+
+	public void printState(Valley valley, ValleyAndExpeditionState state) {
+		for (int y = 0; y <= valley.getMaxY() + 1; y++) {
+			for (int x = 0; x <= valley.getMaxX() + 1; x++) {
+				final Coordinates coordinates = new Coordinates(x, y);
+				if (coordinates.equals(state.getExpeditionLocation())) {
+					System.out.print("E");
+				} else if (state.getBlizzardList().stream().anyMatch(blizzard -> blizzard.getCoordinates().equals(coordinates))) {
+					final List<Blizzard> blizzards = state.getBlizzardList().stream().filter(blizzard -> blizzard.getCoordinates().equals(coordinates)).collect(Collectors.toList());
+					if (blizzards.size() > 1) {
+						System.out.print(blizzards.size());
+					} else {
+						System.out.print(blizzards.get(0).getDirection().getKey());
+					}
+				} else if (!valley.getFields().contains(coordinates)) {
+					System.out.print("#");
+				} else {
+					System.out.print(".");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println();
 	}
 }
